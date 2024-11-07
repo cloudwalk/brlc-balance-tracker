7c6ea6ea9d10fe5ae9a06c4951edee6ea7a3288e
ihoroleksiienko <ihor.oleksiienko@cloudwalk.io>
2024-11-07 17:55:17 +0200

    chore: sync files with all repos

--- scripts/deployProxy.ts
+++ scripts/deployProxy.ts
@@ -1,14 +1,14 @@
 import { ethers, upgrades } from "hardhat";
 
 async function main() {
-  const CONTRACT_NAME: string = ""; // TBD: Enter contract name
+  const CONTRACT_NAME: string = ""; // TODO: Enter contract name
 
   const factory = await ethers.getContractFactory(CONTRACT_NAME);
   const proxy = await upgrades.deployProxy(factory);
 
   await proxy.deployed();
 
-  console.log("Proxy deployed to:", proxy.address);
+  console.log("Proxy deployed to:", await proxy.getAddress());
 }
 
 main().then().catch(err => {
