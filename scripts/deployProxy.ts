dda9d0ee0f5049960cd3c3b8746332541ae064ab
ihoroleksiienko <ihor.oleksiienko@cloudwalk.io>
2024-11-08 16:04:44 +0200

    chore: update scripts

--- scripts/deployProxy.ts
+++ scripts/deployProxy.ts
@@ -1,16 +1,18 @@
 import { ethers, upgrades } from "hardhat";
 
 async function main() {
-  const CONTRACT_NAME: string = ""; // TODO: Enter contract name
+  const CONTRACT_NAME: string = ""; // TBD: Enter contract name
 
   const factory = await ethers.getContractFactory(CONTRACT_NAME);
   const proxy = await upgrades.deployProxy(factory);
 
   await proxy.deployed();
 
-  console.log("Proxy deployed to:", await proxy.getAddress());
+  console.log("Proxy deployed:", proxy.address);
 }
 
-main().then().catch(err => {
-  throw err;
-});
+main()
+  .then()
+  .catch(err => {
+    throw err;
+  });
