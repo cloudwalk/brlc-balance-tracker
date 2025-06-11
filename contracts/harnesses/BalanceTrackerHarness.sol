23590f4ed9fd111af596c4aa2cb7e9d22b5cd54e
Evgenii Zaitsev <evgenii@cloudwalk.io>
2025-06-12 00:10:40 +0700

    fix: add the `missing-initializer` custom NatSpec tags to the harness contracts

--- contracts/harnesses/BalanceTrackerHarness.sol
+++ contracts/harnesses/BalanceTrackerHarness.sol
@@ -9,6 +9,7 @@ import { HarnessAdministrable } from "./HarnessAdministrable.sol";
  * @title BalanceTrackerHarness contract
  * @author CloudWalk Inc. (See https://cloudwalk.io)
  * @notice The same as {BalanceTracker} but with the new functions of setting internal variables for testing
+ * @custom:oz-upgrades-unsafe-allow missing-initializer
  */
 contract BalanceTrackerHarness is BalanceTracker, HarnessAdministrable {
     /// @notice The structure with the contract state
