25269746853179eff1f26e32b76f66b1afe17890
Evgenii Zaitsev <97302011+EvgeniiZaitsevCW@users.noreply.github.com>
2025-06-18 23:30:11 +0700

    docs: improve comments (#10)
    * style: replace the `@notice` tag in comments and add periods
    
    * fix: update contracts version 1.1.0 => 1.1.1
    
    * docs: improve more comments

--- contracts/harnesses/BalanceTrackerHarness.sol
+++ contracts/harnesses/BalanceTrackerHarness.sol
@@ -8,50 +8,50 @@ import { HarnessAdministrable } from "./HarnessAdministrable.sol";
 /**
  * @title BalanceTrackerHarness contract
  * @author CloudWalk Inc. (See https://www.cloudwalk.io)
- * @notice The same as {BalanceTracker} but with additional functions for setting internal variables for testing
+ * @dev The same as {BalanceTracker} but with additional functions for setting internal variables for testing.
  * @custom:oz-upgrades-unsafe-allow missing-initializer
  */
 contract BalanceTrackerHarness is BalanceTracker, HarnessAdministrable {
     // ------------------ Storage layout -------------------------- //
 
-    /// @notice The structure with the contract state
+    /// @dev The structure with the contract state.
     struct BalanceTrackerHarnessState {
         uint256 currentBlockTimestamp;
         bool usingRealBlockTimestamps;
         bool initialized;
     }
 
-    /// @notice The memory slot used to store the contract state
-    /// @dev It is the same as keccak256("balance tracker harness storage slot")
+    /**
+     * @dev The memory slot used to store the contract state.
+     *
+     * It is the same as keccak256("balance tracker harness storage slot").
+     */
     bytes32 private constant _STORAGE_SLOT = 0xceb91ca8f20e7d3bc24614515796ccaa88bb45ed0206676ef6d6620478090c43;
 
     // ------------------ Transactional functions ----------------- //
 
     /**
-     * @notice Sets the initialization day of the balance tracker
-     *
-     * @param day The new initialization day to set
+     * @dev Sets the initialization day of the balance tracker.
+     * @param day The new initialization day to set.
      */
     function setInitializationDay(uint16 day) external onlyOwner {
         INITIALIZATION_DAY = day;
     }
 
     /**
-     * @notice Adds a new balance record to the chronological array for an account
-     *
-     * @param account The address of the account to add the balance record for
-     * @param day The creation day of the new record
-     * @param value The value of the new record
+     * @dev Adds a new balance record to the chronological array for an account.
+     * @param account The address of the account to add the balance record for.
+     * @param day The creation day of the new record.
+     * @param value The value of the new record.
      */
     function addBalanceRecord(address account, uint16 day, uint240 value) external onlyHarnessAdmin {
         _balanceRecords[account].push(Record({ day: day, value: value }));
     }
 
     /**
-     * @notice Sets the balance record chronological array for an account according to the provided array
-     *
-     * @param account The address of the account to set the balance record array for
-     * @param balanceRecords The array of new records to set
+     * @dev Sets the balance record chronological array for an account according to the provided array.
+     * @param account The address of the account to set the balance record array for.
+     * @param balanceRecords The array of new records to set.
      */
     function setBalanceRecords(address account, Record[] calldata balanceRecords) external onlyHarnessAdmin {
         delete _balanceRecords[account];
@@ -62,19 +62,17 @@ contract BalanceTrackerHarness is BalanceTracker, HarnessAdministrable {
     }
 
     /**
-     * @notice Deletes all records from the balance record chronological array for an account
-     *
-     * @param account The address of the account to clear the balance record array for
+     * @dev Deletes all records from the balance record chronological array for an account.
+     * @param account The address of the account to clear the balance record array for.
      */
     function deleteBalanceRecords(address account) external onlyHarnessAdmin {
         delete _balanceRecords[account];
     }
 
     /**
-     * @notice Sets the current block timestamp that should be used by the contract under certain conditions
-     *
-     * @param day The new day index starting from the Unix epoch to set
-     * @param time The new time in seconds starting from the beginning of the day to set
+     * @dev Sets the current block timestamp that should be used by the contract under certain conditions.
+     * @param day The new day index starting from the Unix epoch to set.
+     * @param time The new time in seconds starting from the beginning of the day to set.
      */
     function setBlockTimestamp(uint256 day, uint256 time) external onlyHarnessAdmin {
         BalanceTrackerHarnessState storage state = _getBalanceTrackerHarnessState();
@@ -83,9 +81,8 @@ contract BalanceTrackerHarness is BalanceTracker, HarnessAdministrable {
     }
 
     /**
-     * @notice Sets the boolean variable that defines whether real block timestamps are used in the contract
-     *
-     * @param newValue The new value. If true, real block timestamps are used. Otherwise, previously set ones are used
+     * @dev Sets the boolean variable that defines whether real block timestamps are used in the contract.
+     * @param newValue The new value. If true, real block timestamps are used. Otherwise, previously set ones are used.
      */
     function setUsingRealBlockTimestamps(bool newValue) external onlyOwner {
         BalanceTrackerHarnessState storage state = _getBalanceTrackerHarnessState();
@@ -96,7 +93,7 @@ contract BalanceTrackerHarness is BalanceTracker, HarnessAdministrable {
     // ------------------ View functions -------------------------- //
 
     /**
-     * @notice Returns the boolean value that defines whether real block timestamps are used in the contract
+     * @dev Returns the boolean value that defines whether real block timestamps are used in the contract.
      */
     function getUsingRealBlockTimestamps() external view returns (bool) {
         BalanceTrackerHarnessState storage state = _getBalanceTrackerHarnessState();
@@ -104,7 +101,7 @@ contract BalanceTrackerHarness is BalanceTracker, HarnessAdministrable {
     }
 
     /**
-     * @notice Returns the internal state variable that defines the block timestamp when real timestamps are not used
+     * @dev Returns the internal state variable that defines the block timestamp when real timestamps are not used.
      */
     function getCurrentBlockTimestamp() external view returns (uint256) {
         BalanceTrackerHarnessState storage state = _getBalanceTrackerHarnessState();
@@ -113,7 +110,9 @@ contract BalanceTrackerHarness is BalanceTracker, HarnessAdministrable {
 
     // ------------------ Internal functions ---------------------- //
 
-    /// @notice Returns the block timestamp according to the contract settings: real timestamp or a previously set one
+    /**
+     * @dev Returns the block timestamp according to the contract settings: real timestamp or a previously set one.
+     */
     function _blockTimestamp() internal view virtual override returns (uint256) {
         BalanceTrackerHarnessState storage state = _getBalanceTrackerHarnessState();
         if (state.usingRealBlockTimestamps || !state.initialized) {
@@ -129,7 +128,7 @@ contract BalanceTrackerHarness is BalanceTracker, HarnessAdministrable {
     }
 
     /**
-     * @notice Returns the contract stored state structure
+     * @dev Returns the contract stored state structure.
      */
     function _getBalanceTrackerHarnessState() internal pure returns (BalanceTrackerHarnessState storage) {
         BalanceTrackerHarnessState storage state;
--- contracts/harnesses/HarnessAdministrable.sol
+++ contracts/harnesses/HarnessAdministrable.sol
@@ -7,46 +7,50 @@ import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/O
 /**
  * @title HarnessAdministrable contract
  * @author CloudWalk Inc. (See https://www.cloudwalk.io)
- * @notice Provides the harness admin role for accounts to restrict access to some functions of descendant contracts
- * @dev This contract is used through inheritance. It makes available the modifier `onlyHarnessAdmin`,
+ * @dev Provides the harness admin role for accounts to restrict access to some functions of descendant contracts.
+ *
+ * This contract is used through inheritance. It makes available the modifier `onlyHarnessAdmin`,
  * which can be applied to functions to restrict their usage.
- * This contract uses a pseudo-random storage slot to save its state, so no storage layout control is needed
+ *
+ * This contract uses a pseudo-random storage slot to save its state, so no storage layout control is needed.
+ *
  * @custom:oz-upgrades-unsafe-allow missing-initializer
  */
 abstract contract HarnessAdministrable is OwnableUpgradeable {
     // ------------------ Storage layout -------------------------- //
 
-    /// @notice The structure with the contract state
+    /// @dev The structure with the contract state.
     struct HarnessAdministrableState {
         mapping(address => bool) harnessAdminStatuses;
     }
 
-    /// @notice The memory slot used to store the contract state
-    /// @dev It is the same as keccak256("harness administrable storage slot")
+    /**
+     * @dev The memory slot used to store the contract state.
+     *
+     * It is the same as keccak256("harness administrable storage slot").
+     */
     bytes32 private constant _STORAGE_SLOT = 0xfe59a931f94e2aa9825bd975f0e041e1561aab13eea3c8ef2be9da7a34db16e2;
 
     // ------------------ Events ---------------------------------- //
 
     /**
-     * @notice Emitted when configuration of a harness admin is updated
-     *
-     * @param harnessAdmin The address of the configured harness admin
-     * @param status The new status of the harness admin
+     * @dev Emitted when configuration of a harness admin is updated.
+     * @param harnessAdmin The address of the configured harness admin.
+     * @param status The new status of the harness admin.
      */
     event HarnessAdminConfigured(address indexed harnessAdmin, bool status);
 
     // ------------------ Errors ---------------------------------- //
 
     /**
-     * @notice The transaction sender is not a harness admin
-     *
-     * @param account The address of the transaction sender
+     * @dev The transaction sender is not a harness admin.
+     * @param account The address of the transaction sender.
      */
     error UnauthorizedHarnessAdmin(address account);
 
     // ------------------ Modifiers ------------------------------- //
 
-    /// @notice Throws if called by any account other than a harness admin
+    /// @dev Throws if called by any account other than a harness admin.
     modifier onlyHarnessAdmin() {
         _checkHarnessAdmin(_msgSender());
         _;
@@ -55,16 +59,16 @@ abstract contract HarnessAdministrable is OwnableUpgradeable {
     // ------------------ Transactional functions ----------------- //
 
     /**
-     * @notice Updates configuration of a harness admin
+     * @dev Updates configuration of a harness admin.
      *
      * Requirements:
      *
-     * - Can only be called by the contract owner
+     * - Can only be called by the contract owner.
      *
-     * Emits a {HarnessAdminConfigured} event if the configuration was changed
+     * Emits a {HarnessAdminConfigured} event if the configuration was changed.
      *
-     * @param account The address of the harness admin to be configured
-     * @param newStatus The new status of the harness admin
+     * @param account The address of the harness admin to be configured.
+     * @param newStatus The new status of the harness admin.
      */
     function configureHarnessAdmin(address account, bool newStatus) external onlyOwner {
         _configureHarnessAdmin(account, newStatus);
@@ -73,10 +77,9 @@ abstract contract HarnessAdministrable is OwnableUpgradeable {
     // ------------------ View functions -------------------------- //
 
     /**
-     * @notice Checks if an account is a harness admin
-     *
-     * @param account The account to check for the harness admin configuration
-     * @return True if the account is a configured harness admin, False otherwise
+     * @dev Checks if an account is a harness admin.
+     * @param account The account to check for the harness admin configuration.
+     * @return True if the account is a configured harness admin, False otherwise.
      */
     function isHarnessAdmin(address account) external view returns (bool) {
         return _isHarnessAdmin(account);
@@ -85,12 +88,12 @@ abstract contract HarnessAdministrable is OwnableUpgradeable {
     // ------------------ Internal functions ---------------------- //
 
     /**
-     * @notice Updates configuration of a harness admin internally
+     * @dev Updates configuration of a harness admin internally.
      *
-     * Emits a {HarnessAdminConfigured} event if the configuration was changed
+     * Emits a {HarnessAdminConfigured} event if the configuration was changed.
      *
-     * @param account The address of the harness admin to be configured
-     * @param newStatus The new status of the harness admin
+     * @param account The address of the harness admin to be configured.
+     * @param newStatus The new status of the harness admin.
      */
     function _configureHarnessAdmin(address account, bool newStatus) internal {
         HarnessAdministrableState storage state = _getHarnessAdministrableState();
@@ -103,9 +106,8 @@ abstract contract HarnessAdministrable is OwnableUpgradeable {
     }
 
     /**
-     * @notice Checks if an account is a harness admin internally and reverts if not
-     *
-     * @param account The account to check for the harness admin configuration
+     * @dev Checks if an account is a harness admin internally and reverts if not.
+     * @param account The account to check for the harness admin configuration.
      */
     function _checkHarnessAdmin(address account) internal view {
         if (!_isHarnessAdmin(account)) {
@@ -114,10 +116,9 @@ abstract contract HarnessAdministrable is OwnableUpgradeable {
     }
 
     /**
-     * @notice Checks if an account is a harness admin internally
-     *
-     * @param account The account to check for the harness admin configuration
-     * @return True if the account is a configured harness admin, False otherwise
+     * @dev Checks if an account is a harness admin internally.
+     * @param account The account to check for the harness admin configuration.
+     * @return True if the account is a configured harness admin, False otherwise.
      */
     function _isHarnessAdmin(address account) internal view returns (bool) {
         HarnessAdministrableState storage state = _getHarnessAdministrableState();
@@ -125,7 +126,7 @@ abstract contract HarnessAdministrable is OwnableUpgradeable {
     }
 
     /**
-     * @notice Returns the contract stored state structure
+     * @dev Returns the contract stored state structure.
      */
     function _getHarnessAdministrableState() internal pure returns (HarnessAdministrableState storage) {
         HarnessAdministrableState storage state;
